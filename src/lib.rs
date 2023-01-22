2635a3952e90830c7b8642b78a4bbfee852300d2
Patrik Kormosi <pkormosi@frinx.io>
2023-01-22 21:20:59 +0100

    Add argument parsing

--- src/lib.rs
+++ src/lib.rs
@@ -1,29 +1,87 @@
 use anyhow::{Ok, Result};
-use socket2::{Domain, Protocol, SockAddr, Socket, Type};
+use clap::Parser;
+use socket2::{Domain, Protocol, Socket, Type};
+use std::io::Read;
 use std::net::{Ipv4Addr, SocketAddrV4};
+use std::str::FromStr;
+
+#[derive(Parser)]
+#[command(name = "Ring - a ping implementation in Rust")]
+#[command(version = "0.0.1")]
+#[command(about = "Sends ICMP echo packets to hosts", long_about = None)]
+struct Cli {
+    /// IPv4 address to operate on, defaults to 127.0.0.1
+    address: Option<String>,
+
+    /// Stop after <count> replies, defaults to 5
+    #[arg(short, long, action = clap::ArgAction::Set)]
+    count: Option<u8>,
+}
+
+struct IcmpEchoHeader {
+    type_of_message: [u8; 1],
+    code: [u8; 1],
+    checksum: [u8; 2],
+    identifier: [u8; 2],
+    sequence_number: [u8; 2],
+}
+
+impl IcmpEchoHeader {
+    fn to_vec(self) -> Vec<u8> {
+        let mut tmp_vec = Vec::with_capacity(8);
+        tmp_vec.extend_from_slice(&self.type_of_message);
+        tmp_vec.extend_from_slice(&self.code);
+        tmp_vec.extend_from_slice(&self.checksum);
+        tmp_vec.extend_from_slice(&self.identifier);
+        tmp_vec.extend_from_slice(&self.sequence_number);
+        tmp_vec
+    }
+}
+
+impl Default for IcmpEchoHeader {
+    fn default() -> Self {
+        IcmpEchoHeader {
+            type_of_message: *b"\x08",
+            code: *b"\x00",
+            checksum: *b"\xe5\xca",
+            identifier: *b"\x12\x34",
+            sequence_number: *b"\x00\x01",
+        }
+    }
+}
 
 pub fn run() -> Result<u8> {
-    // Build IP header
-    let mut ip_header = b"\x45\x00\x00\x1c".to_vec(); // IP version | IHL | Type of service | Total length
-    ip_header.extend_from_slice(b"\xab\xcd\x00\x00"); // Identification | Flags | Fragment Offset
-    ip_header.extend_from_slice(b"\x40\x01\xd1\x11"); // TTL | Protocol (ICMP) | Header checksum
-    ip_header.extend_from_slice(b"\x7f\x00\x00\x01"); // SRC address
-    ip_header.extend_from_slice(b"\x7f\x00\x00\x01"); // DST address
+    let cli = Cli::parse();
 
-    // Build ICMP header
-    let mut icmp_header = b"\x08\x00\xe5\xca".to_vec(); // Type of message, Code | Checksum
-    icmp_header.extend_from_slice(b"\x12\x34\x00\x01"); // Identifier | Sequence Number
+    // Parse the arguments
+    let request_count = match cli.count {
+        Some(count) => count,
+        None => 5,
+    };
 
-    // Concatenate the above
-    ip_header.extend_from_slice(icmp_header.as_slice());
+    let address = match cli.address {
+        Some(a) => a.parse()?,
+        None => Ipv4Addr::LOCALHOST,
+    };
 
-    // println!("{:?}", ip_header);
+    // Build the ICMP header
+    let icmp_header = IcmpEchoHeader::default().to_vec();
 
-    let address = SockAddr::from(SocketAddrV4::new(Ipv4Addr::new(127, 0, 0, 1), 0));
-    let socket = Socket::new(Domain::IPV4, Type::RAW, Some(Protocol::ICMPV4))?;
-    socket.set_header_included(true)?;
+    // Create the socket
+    let mut socket = Socket::new(Domain::IPV4, Type::RAW, Some(Protocol::ICMPV4))?;
 
-    socket.send_to(ip_header.as_slice(), &address)?;
+    // Send the ICMP header
+    for _ in 1..=request_count {
+        // println!("Sending ICMP echo packet to {}", cli.address.as_ref().unwrap());
+
+        socket.send_to(
+            &icmp_header.as_slice(),
+            &SocketAddrV4::new(address, 0).into(),
+        )?;
+        let mut response_buffer: [u8; 28] = [0; 28];
+        socket.read(response_buffer.as_mut_slice())?;
+        println!("{response_buffer:x?}");
+    }
 
     Ok(0)
 }
